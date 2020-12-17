ab826854bd2b28f8f756916c9df8f80fd5ccdbb8
yishian.yu <yishian.yu@gmail.com>
2020-12-17 16:35:35 +0800

    1217 first edit

---
+++ js/all.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//     var scrollPos = $(window).scrollTop();
+//     // console.log(scrollPos);
+      
+//     $('#scroll-ele').css('transform','translateY('+(scrollPos/2)+'px)') 
+//     })
+// });
