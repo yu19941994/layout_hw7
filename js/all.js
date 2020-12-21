78da0b2133a060eb356414534deb4f097ad4a690
yu19941994 <yishian.yu@gmail.com>
2020-12-21 14:03:22 +0800

    修改助教建議

--- js/all.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//     var scrollPos = $(window).scrollTop();
-//     // console.log(scrollPos);
-      
-//     $('#scroll-ele').css('transform','translateY('+(scrollPos/2)+'px)') 
-//     })
-// });
